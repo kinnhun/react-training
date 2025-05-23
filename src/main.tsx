91aec1d9c739e62149cd6cc8edf66084af20995f
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-23 16:48:05 +0700

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
- 
+import React from 'react'; // ✅ BẮT BUỘC với StrictMode
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
