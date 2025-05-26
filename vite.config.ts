1b897f9ffcc210eac30b52a84db6fd25eec623e6
Trần Trung Kiên <trungkien2981412@gmail.com>
2025-05-26 09:56:42 +0700

    cicd

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import * as path from 'path' 
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
-  base: '/react-training/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), 
-      },
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
